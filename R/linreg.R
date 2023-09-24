12725508b28ac4e4e224f84929a032f74b56f0d6
Siddhesh Sreedar <siddheshraw@gmail.com>
2023-09-24 18:44:08 +0200

    With documentation

--- R/linreg.R
+++ R/linreg.R
@@ -1,12 +1,23 @@
 #Installed packages: library(stringr), library(ggplot2)
 
-#linreg<-function(formula,data){
   
-  #X <- model.matrix(formula,data)
-  
-  #y <- all.vars(formula)
-  
-  
+#' Linear Regression
+#'
+#' @field formula defines a formula of how we want to model the relationship between different variables. 
+#' @field data It is a dataframe containing multiple variables in the columns and different observations of those variables in the rows.  
+#' @field coeff Regressions coefficients. 
+#' @field fitted The fitted values.  
+#' @field residuals The residuals.  
+#' @field dof The degrees of freedom.  
+#' @field rvariance The residual variance. 
+#' @field varcoeff The variance of the regression coefficients. 
+#' @field tvalues The t-values for each coefficient.  
+#' 
+#' 
+#' @return A class with multiple fields and methods to use on. 
+#' @export
+#'
+#' 
   linreg<-setRefClass("linreg",
                       fields = list(formula="formula",data="data.frame",d = "character",X="matrix",y="vector",coeff="matrix",fitted="matrix",
                                     residuals="matrix", dof="numeric", rvariance="matrix", varcoeff="matrix", tvalues="matrix"),
@@ -32,7 +43,7 @@
                           names(a)<-row.names(coeff)
                           e<- as.character(formula)
                           e<-paste(e[2],e[3])
-                          c<- str_glue("lm(formula = {e}, data = {d})")
+                          c<- stringr::str_glue("lm(formula = {e}, data = {d})")
                           b<-list(Call = c , Coefficients = a)
                           return(b)
                         },
@@ -43,8 +54,8 @@
                           names(c)<-c("Residuals","Fitted")
                           e<- as.character(formula)
                           e<-paste(e[2],e[3])
-                          d<<- str_glue("Fitted values lm({e})", .sep = "\n")
-                          ggplot(c,aes(Fitted,Residuals))+geom_point()+labs(title="Residuals vs Fitted", x = d)
+                          d<<- stringr::str_glue("Fitted values lm({e})", .sep = "\n")
+                          ggplot2::ggplot(c,aes(Fitted,Residuals))+geom_point()+labs(title="Residuals vs Fitted", x = d)
                         },
                         resid = function(){
                           return(residuals)
@@ -62,7 +73,7 @@
                           names(a)<-row.names(coeff)
                           e<- as.character(formula)
                           e<-paste(e[2],e[3])
-                          c<- str_glue("lm(formula = {e}, data = {d})")
+                          c<- stringr::str_glue("lm(formula = {e}, data = {d})")
                           b<-list(Call = c , Coefficients = a, tvalue = tvalues, p_value = " " ,StandardDev = rvariance, DegreeOfFreedom = dof)
                           return(b)
                         }
@@ -71,8 +82,4 @@
   
             
   
-  l1<-linreg(formula = Petal.Length~Species + Petal.Width, data = iris)
-  
-  #return(l1)
-  
-#}
+  #l1<-linreg(formula = Petal.Length~Species + Petal.Width, data = iris)
